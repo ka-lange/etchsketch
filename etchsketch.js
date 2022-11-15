f736c25483ebfff95a77ee26e75638aaaf5c1f34
Karissa <ka.lange266@gmail.com>
2022-11-15 13:29:21 -0500

    css created to define square area for added pixels

--- etchsketch.js
+++ etchsketch.js
@@ -1,8 +1,6 @@
 var pixel = document.createElement('div');
-    pixel.style.width = '50px';
-    pixel.style.height = '50px';
+    pixel.style.width = '25px';
+    pixel.style.height = '25px';
     pixel.style.border = 'solid black'
 var square = document.getElementById('square');
-square.appendChild(pixel);
-square.appendChild(pixel);
 square.appendChild(pixel);
