316741c247a30eb70eeecf5cf82878d09649f687
Karissa <ka.lange266@gmail.com>
2022-11-15 13:21:58 -0500

    create container div to hold new squares, js set up to create and append single pixel square

---
+++ etchsketch.js
@@ -0,0 +1,8 @@
+var pixel = document.createElement('div');
+    pixel.style.width = '50px';
+    pixel.style.height = '50px';
+    pixel.style.border = 'solid black'
+var square = document.getElementById('square');
+square.appendChild(pixel);
+square.appendChild(pixel);
+square.appendChild(pixel);
