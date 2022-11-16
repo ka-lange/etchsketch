ab98dde9775e9eaf8d9a07dfe040944354a351d3
Karissa <ka.lange266@gmail.com>
2022-11-16 10:58:50 -0500

    begin writing grid function with nesting functions appending rows to columns to main grid

--- etchsketch.js
+++ etchsketch.js
@@ -1,6 +1,25 @@
-var pixel = document.createElement('div');
+/*var pixel = document.createElement('div');
     pixel.style.width = '25px';
     pixel.style.height = '25px';
     pixel.style.border = 'solid black'
 var square = document.getElementById('square');
-square.appendChild(pixel);
+square.append(pixel); */
+
+
+    var grid = document.getElementById('grid');
+    var columns = 4;
+    var rows = 4;
+
+    for(var i = 0; i < columns; i++) {
+        var column = document.createElement('div'); //create column
+        column.className = 'column';
+        for (var j=0; j < rows; j++) {
+            var row = document.createElement('div'); //create row
+            row.style.border = 'blue';
+            row.className = 'gridsquare';
+            row.textContent = i + '-' + j;  //set text (?)
+            column.appendChild(row);  //append row in column
+        }
+        grid.appendChild(column); //append column (with rows) inside grid
+    }
+    
