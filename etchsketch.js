5070d746128b29357bcce009ae38761c2ef5c2c8
Karissa <ka.lange266@gmail.com>
2022-11-16 13:14:56 -0500

    add event listener that adds class when a gridsquare div is hovered over

--- etchsketch.js
+++ etchsketch.js
@@ -8,21 +8,23 @@ for(var i = 0; i < columns; i++) {
     for (var j=0; j < rows; j++) {
         var row = document.createElement('div'); //create row
         row.classList.add('gridsquare');
-        row.textContent = i + '-' + j;  //set text placeholders for the divs
+        row.textContent = '.';  //set text placeholders for the divs
         column.appendChild(row);  //append row in column
-        
-        row.addEventListener('mouseover', colorChange)
-        function colorChange () {
-            row.setAttribute("style", "background-color: black;")
-        }
     }
     grid.appendChild(column); //append column (with rows) inside grid   
 }
 
-//Set up a “hover” effect so that the grid divs change color 
-//when your mouse passes over them, 
-//leaving a (pixelated) trail through your grid
-//like a pen would.
+//this below adds a class to all the new divs when hovered 
+//and the css file targets the new class
+grid.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "gridsquare" class
+    if (e.target.matches('.gridsquare')) {
+      e.target.classList.add('active');
+    }
+  });
+  
+
+
 
 
 
